fcab6996a58a03be387f0bb823be83bd76c1dd3a
Jason Virtue <virtuej@gmail.com>
2024-02-13 18:20:08 -0500

    Script to run models

---
+++ object_detection_script.py
@@ -0,0 +1,220 @@
+#object_detection_script
+
+import collections
+import json
+import tarfile
+import time
+from pathlib import Path
+import videoplayer as utils
+
+#Requires headless version of opencv-python pip install opencv-python-headless
+import cv2
+import numpy as np
+from IPython import display
+import openvino as ov
+from openvino.tools.mo.front import tf as ov_tf_front
+from openvino.tools import mo
+
+precision = "FP16"
+model_name = "ssdlite_mobilenet_v2"
+
+# The output path for the conversion.
+converted_model_path = Path("model") / f"{model_name}_{precision.lower()}.xml"
+core = ov.Core()
+
+# Read the network and corresponding weights from a file.
+model = core.read_model(model=converted_model_path)
+
+# Compile the model for CPU (you can choose manually CPU, GPU etc.)
+compiled_model = core.compile_model(model=model, device_name='CPU')
+
+# Get the input and output nodes.
+input_layer = compiled_model.input(0)
+output_layer = compiled_model.output(0)
+
+# Get the input size.
+height, width = list(input_layer.shape)[1:3]
+input_layer.any_name, output_layer.any_name
+
+# https://tech.amikelive.com/node-718/what-object-categories-labels-are-in-coco-dataset/
+classes = [
+    "background", "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train",
+    "truck", "boat", "traffic light", "fire hydrant", "street sign", "stop sign",
+    "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant",
+    "bear", "zebra", "giraffe", "hat", "backpack", "umbrella", "shoe", "eye glasses",
+    "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite",
+    "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle",
+    "plate", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+    "couch", "potted plant", "bed", "mirror", "dining table", "window", "desk", "toilet",
+    "door", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven",
+    "toaster", "sink", "refrigerator", "blender", "book", "clock", "vase", "scissors",
+    "teddy bear", "hair drier", "toothbrush", "hair brush"
+]
+
+# Colors for the classes above (Rainbow Color Map).
+colors = cv2.applyColorMap(
+    src=np.arange(0, 255, 255 / len(classes), dtype=np.float32).astype(np.uint8),
+    colormap=cv2.COLORMAP_RAINBOW,
+).squeeze()
+
+def process_results(frame, results, thresh=0.6):
+    # The size of the original frame.
+    h, w = frame.shape[:2]
+    # The 'results' variable is a [1, 1, 100, 7] tensor.
+    results = results.squeeze()
+    boxes = []
+    labels = []
+    scores = []
+    for _, label, score, xmin, ymin, xmax, ymax in results:
+        # Create a box with pixels coordinates from the box with normalized coordinates [0,1].
+        boxes.append(
+            tuple(map(int, (xmin * w, ymin * h, (xmax - xmin) * w, (ymax - ymin) * h)))
+        )
+        labels.append(int(label))
+        scores.append(float(score))
+
+    # Apply non-maximum suppression to get rid of many overlapping entities.
+    # See https://paperswithcode.com/method/non-maximum-suppression
+    # This algorithm returns indices of objects to keep.
+    indices = cv2.dnn.NMSBoxes(
+        bboxes=boxes, scores=scores, score_threshold=thresh, nms_threshold=0.6
+    )
+
+    # If there are no boxes.
+    if len(indices) == 0:
+        return []
+
+    # Filter detected objects.
+    return [(labels[idx], scores[idx], boxes[idx]) for idx in indices.flatten()]
+
+def draw_boxes(frame, boxes):
+    for label, score, box in boxes:
+        # Choose color for the label.
+        color = tuple(map(int, colors[label]))
+        # Draw a box.
+        x2 = box[0] + box[2]
+        y2 = box[1] + box[3]
+        cv2.rectangle(img=frame, pt1=box[:2], pt2=(x2, y2), color=color, thickness=3)
+
+        # Draw a label name inside the box.
+        cv2.putText(
+            img=frame,
+            text=f"{classes[label]} {score:.2f}",
+            org=(box[0] + 10, box[1] + 30),
+            fontFace=cv2.FONT_HERSHEY_COMPLEX,
+            fontScale=frame.shape[1] / 1000,
+            color=color,
+            thickness=1,
+            lineType=cv2.LINE_AA,
+            )
+    
+    return frame
+
+# Main processing function to run object detection.
+def run_object_detection(source=0, flip=False, use_popup=False, skip_first_frames=0):
+    player = None
+    try:
+        # Create a video player to play with target fps.
+        player = utils.VideoPlayer(
+            source=source, flip=flip, fps=30, skip_first_frames=skip_first_frames
+        )
+        # Start capturing.
+        player.start()
+        if use_popup:
+            title = "Press ESC to Exit"
+            cv2.namedWindow(
+                winname=title, flags=cv2.WINDOW_GUI_NORMAL | cv2.WINDOW_AUTOSIZE
+            )
+
+        processing_times = collections.deque()
+        while True:
+            # Grab the frame.
+            frame = player.next()
+            if frame is None:
+                print("Source ended")
+                break
+            # If the frame is larger than full HD, reduce size to improve the performance.
+            scale = 1280 / max(frame.shape)
+            if scale < 1:
+                frame = cv2.resize(
+                    src=frame,
+                    dsize=None,
+                    fx=scale,
+                    fy=scale,
+                    interpolation=cv2.INTER_AREA,
+                )
+
+            # Resize the image and change dims to fit neural network input.
+            input_img = cv2.resize(
+                src=frame, dsize=(width, height), interpolation=cv2.INTER_AREA
+            )
+            # Create a batch of images (size = 1).
+            input_img = input_img[np.newaxis, ...]
+
+            # Measure processing time
+            start_time = time.time()
+            # Get the results.
+            results = compiled_model([input_img])[output_layer]
+            stop_time = time.time()
+            # Get poses from network results.
+            boxes = process_results(frame=frame, results=results)
+            with open("data_file.json", "a") as write_file:
+                json.dump(boxes, write_file)
+
+            # Draw boxes on a frame.
+            frame = draw_boxes(frame=frame, boxes=boxes)
+
+            processing_times.append(stop_time - start_time)
+            # Use processing times from last 200 frames.
+            if len(processing_times) > 200:
+                processing_times.popleft()
+
+            _, f_width = frame.shape[:2]
+            # Mean processing time [ms].
+            processing_time = np.mean(processing_times) * 1000
+            fps = 1000 / processing_time
+            cv2.putText(
+                img=frame,
+                text=f"Inference time: {processing_time:.1f}ms ({fps:.1f} FPS)",
+                org=(20, 40),
+                fontFace=cv2.FONT_HERSHEY_COMPLEX,
+                fontScale=f_width / 1000,
+                color=(0, 0, 255),
+                thickness=1,
+                lineType=cv2.LINE_AA,
+            )
+
+            # Use this workaround if there is flickering.
+            if use_popup:
+                cv2.imshow(winname=title, mat=frame)
+                key = cv2.waitKey(1)
+                # escape = 27
+                if key == 27:
+                    break
+            else:
+                # Encode numpy array to jpg.
+                _, encoded_img = cv2.imencode(
+                    ext=".jpg", img=frame, params=[cv2.IMWRITE_JPEG_QUALITY, 100]
+                )
+                # Create an IPython image.
+                i = display.Image(data=encoded_img)
+                # Display the image in this notebook.
+                display.clear_output(wait=True)
+                display.display(i)
+         
+    # ctrl-c
+    except KeyboardInterrupt:
+        print("Interrupted")
+    # any different error
+    except RuntimeError as e:
+        print(e)
+    finally:
+        if player is not None:
+            # Stop capturing.
+            player.stop()
+        if use_popup:
+            cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    run_object_detection(source=0, flip=isinstance(0, int), use_popup=True)
